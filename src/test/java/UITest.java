ffb9f5e2775ed9c5962e96251037c36e28d4eb1c
mduuude <mduuude@gmail.com>
2017-11-08 19:43:54 +0200

    refactoring #OTP-5

--- src/test/java/UITest.java
+++ src/test/java/UITest.java
@@ -1,20 +1,19 @@
 import database.DBExecutor;
 import io.github.bonigarcia.wdm.ChromeDriverManager;
 import model.Employee;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.*;
+import pages.MainPageObject;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 public class UITest {
 
     private WebDriver driver;
+    private MainPageObject mainPage;
     private static final String BASE_URL = "http://benzoelectromoty.ga/qa/";
 
     @BeforeClass
@@ -31,30 +30,16 @@ public class UITest {
     public void getUIData() throws IllegalAccessException, SQLException {
         driver.get(BASE_URL);
 
-        List<WebElement> trList = driver.findElements(By.xpath("/html/body/table/tbody/tr"));
+        mainPage = new MainPageObject(driver);
         List<Employee> dBEmployeesList = new DBExecutor().getDataListFromEmployee();
-        List<Employee> uIEmployeesList = new ArrayList<Employee>();
-
-        for (int i = 0; i < trList.size(); i++) {
-            List<WebElement> propsList = driver.findElements(By.xpath("/html/body/table/tbody/tr[" + i + "]/td"));
-            if (propsList.size() == 5) {
-                Employee employee = new Employee();
-
-                employee.setId(Integer.parseInt(propsList.get(0).getText()));
-                employee.setDepartmentId(Integer.parseInt(propsList.get(1).getText()));
-                employee.setChiefId(Integer.parseInt(propsList.get(2).getText()));
-                employee.setName(propsList.get(3).getText());
-                employee.setSalary(Integer.parseInt(propsList.get(4).getText()));
-
-                uIEmployeesList.add(employee);
-            }
-        }
+        List<Employee> uIEmployeesList = mainPage.getEmployeesList();
 
         for (int i = 0; i < dBEmployeesList.size(); i++) {
             Employee dBEmployee = dBEmployeesList.get(i);
             Employee uIEmployee = uIEmployeesList.get(i);
 
-            Assert.assertTrue(dBEmployee.equals(uIEmployee), "Entity from DB with id=" + dBEmployee.getId() + " is not equal to UI entity");
+            Assert.assertTrue(dBEmployee.equals(uIEmployee),
+                    "Entity from DB with id=" + dBEmployee.getId() + " is not equal to UI entity");
         }
     }
 
